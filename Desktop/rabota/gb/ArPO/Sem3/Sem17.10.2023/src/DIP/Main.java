19127170f54443d08129938cbcad1b608a6bb22c
Кирилл <kilkuruzzza@mail.ru>
2024-03-27 03:41:20 +0300

    First commit

---
+++ Desktop/rabota/gb/ArPO/Sem3/Sem17.10.2023/src/DIP/Main.java
@@ -0,0 +1,8 @@
+package DIP;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(new PetrolEngine());
+        car.start();
+    }
+}
---
+++ source/repos/TestWayToA/TestWayToA/TestWayToA.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    //Цикл для перебора наборов данных
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        //Нулевой массив на n элементов. 
+        vector<int> binWhite(n, 0);
+        for (int i = 0; i < n; i++) {
+            //В массиве binWhite отмечаем 1 белую клетку  
+            binWhite[i] = (s[i] == 'W' ? 1 : 0);
+        }
+        // Переменная для минимального перекрашивания клеток. Изначально предполагаем что нужно перекрасить всю строку
+        int minRecolor = s.length();
+        // Цикл для перебора всех подпоследовательностей
+        for (int i = 0; i + k - 1 < n; i++) {
+            // Считаем белые клетки, начиная с текущего элемента и на k элементов вперёд
+            int ans = accumulate(binWhite.begin() + i, binWhite.begin() + i + k, 0);
+            // Сравниваем наше значение с минимальным количеством перекрашиваний
+            if(minRecolor > ans)
+            minRecolor = ans;
+        }
+        cout << minRecolor << endl;
+           
+    }
+
+    return 0;
+}
